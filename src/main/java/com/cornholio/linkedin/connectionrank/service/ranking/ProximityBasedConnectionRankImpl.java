e56fc6e29ffac227bd6428a191609e424bf51ec5
Nikhil Agarwal <agarwal.nikhil85@gmail.com>
2015-11-13 10:22:32 -0800

    Remove guava collections and use Java streams

--- src/main/java/com/cornholio/linkedin/connectionrank/service/ranking/ProximityBasedConnectionRankImpl.java
+++ src/main/java/com/cornholio/linkedin/connectionrank/service/ranking/ProximityBasedConnectionRankImpl.java
@@ -1,10 +1,10 @@
 package com.cornholio.linkedin.connectionrank.service.ranking;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import com.cornholio.linkedin.connectionrank.model.Connections;
 import com.cornholio.linkedin.connectionrank.model.Person;
-import com.google.common.collect.Ordering;
-
-import java.util.List;
 
 /**
  * Sorting is based on industry and location of the connection with respect to the user. Ideally one would use some sort of mechanism to determine the
@@ -35,7 +35,11 @@ public class ProximityBasedConnectionRankImpl implements ConnectionRankService {
         sortedConnections.setCount(connections.getCount());
         sortedConnections.setStart(connections.getStart());
         sortedConnections.setTotal(connections.getTotal());
-        final List<Person> people = Ordering.from(new DefaultProximityComparator(user)).reverse().sortedCopy(connections.getPersons());
+        final List<Person> people = connections.getPersons()
+        		.stream()
+        		.sorted(new DefaultProximityComparator(user)
+        				.reversed())
+        		.collect(Collectors.toList());
         sortedConnections.setPersons(people);
         return sortedConnections;
     }
